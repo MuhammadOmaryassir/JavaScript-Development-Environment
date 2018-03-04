b22a33aceb127d77129c603b27820d6e19a6aa62
Omaroovee <Muhammad.omar.eg@gmail.com>
2018-03-04 13:55:14 +0200

    implement static files to test webpack bundler

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -25,7 +25,7 @@ app.listen(port, function(err){
   if (err) {
     console.log(err)
   } else {
-    open('http://localhost:' + port, "firefox");
+    open('http://localhost:' + port );
   }
 })
 
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './index.css'
+
+import numeral from 'numeral'
+
+const courseValue = numeral(1000).format('$0,0,00')
+//add (debugger) to debug the ES6 Code via Source Maps
+console.log('I would Pay ' + courseValue + ' for this awesome course.')
