f9cc553ad5f22ec15c8145971a907c80b99106e9
Omaroovee <Muhammad.omar.eg@gmail.com>
2018-03-04 20:31:23 +0200

    generate fake users data and make api to get it and delete it

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
-import './index.css'
+import './index.css';
 
-/* eslint-disable no-console */
+import {getUsers, deleteUser} from './api/userApi';
 
+// Populate table of users via API call.
+getUsers().then(result => {
+  let usersBody = "";
 
+  result.forEach(user => {
+    usersBody+= `<tr>
+      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
+      </tr>`
+  });
 
+  global.document.getElementById('users').innerHTML = usersBody;
 
-import numeral from 'numeral'
+  const deleteLinks = global.document.getElementsByClassName('deleteUser');
 
-const courseValue = numeral(1000).format('$0,0,00')
-//add (debugger) to debug the ES6 Code via Source Maps
-console.log('I would Pay ' + courseValue + ' for this awesome course.')
+  // Must use array.from to create a real array from a DOM collection
+  // getElementsByClassname only returns an "array like" object
+  Array.from(deleteLinks, link => {
+    link.onclick = function(event) {
+      const element = event.target;
+      event.preventDefault();
+      deleteUser(element.attributes["data-id"].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    };
+  });
+});
